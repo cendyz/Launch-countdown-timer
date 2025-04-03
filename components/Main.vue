75200195afe4d53b5723c3f3bce2143f571191aa
cendyz <141049293+cendyz@users.noreply.github.com>
2025-04-03 17:07:46 +0200

    design update

--- components/Main.vue
+++ components/Main.vue
@@ -1,13 +1,13 @@
 <template>
 	<main class="overflow-x-clip flex flex-col justify-center items-center h-[50rem] text-center px-[3rem]">
-		<h1 class="text-neutral-white uppercase text-[2rem] tracking-[.6rem] leading-[1.3] md:text-[2.7rem] lg:text-[3rem]">
+		<h1 class="text-neutral-white uppercase text-[2rem] tracking-[.6rem] leading-[1.3] md:text-[2.7rem] lg:text-[2rem]">
 			we're launching soon
 		</h1>
 		<div class="mt-[5rem] flex justify-center gap-x-[1.8rem] w-full">
 			<div class="" v-for="(item, index) in timerData" :key="index">
 				<div
-					class="w-[6.5rem] h-[6.5rem] grid place-items-center relative before:absolute before:top-0 before:left-0 before:w-full before:h-[50%] before:bg-primary-blue before:rounded-lg after:absolute after:bottom-0 after:left-0 after:w-full after:h-[50%] after:bg-primary-blue after:rounded-lg before:opacity-[10%] after:opacity-20 md:w-[7.5rem] md:h-[7.5rem]">
-					<p class="text-primary-red text-[3rem] md:text-[4rem] lg:text-[4.5rem]">
+					class="w-[6.5rem] h-[6.5rem] grid place-items-center relative before:absolute before:top-0 before:left-0 before:w-full before:h-[50%] before:bg-primary-blue before:rounded-lg after:absolute after:bottom-0 after:left-0 after:w-full after:h-[50%] after:bg-primary-blue after:rounded-lg before:opacity-[10%] after:opacity-20 md:w-[7.5rem] md:h-[7.5rem] lg:w-[13rem] lg:h-[13rem]">
+					<p class="text-primary-red text-[3rem] md:text-[4rem] lg:text-[6.5rem]">
 						{{ item.time < 10 ? `${zero}${item.time}` : item.time }}
 					</p>
 				</div>
